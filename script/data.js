292cd21c207e8e93bcaa2475d2264b7e9847b8a3
Luna222 <thao@ordinaryfolk.health>
2023-12-30 02:10:57 +0700

    up

--- script/data.js
+++ script/data.js
@@ -2,11 +2,38 @@
 /*******************************************************************************
  * Variables
  ******************************************************************************/
+//select DOM elements
+const btnImport = document.querySelector('#import-btn');
+const btnExport = document.querySelector('#export-btn');
 
 /*******************************************************************************
  * Functions
  ******************************************************************************/
+/**
+ * @brief save the JSON data as a file
+ *
+ * @param {Array} data - in JSON-like format
+ * @param {Object} fileType
+ * @param {String} fileName
+ */
+const saveDataToFile = function (data, fileType, fileName) {
+  const blob = new Blob([JSON.stringify(data)], fileType);
+  saveAs(blob, fileName);
+};
 
 /*******************************************************************************
  * Handle Events
  ******************************************************************************/
+/**
+ * @brief handle exporting Pets event
+ */
+btnExport.addEventListener('click', function (e) {
+  //prevent the page from re-loading after hitting button
+  e.preventDefault();
+
+  saveDataToFile(
+    petArr,
+    { type: 'application/json;charset=utf-8' },
+    'pets.json'
+  );
+});
